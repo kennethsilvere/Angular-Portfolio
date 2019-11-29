2b38be696e47fc029283eaca0f80605bbcf793d8
Kenneth Silvere <kennethsilvere93@gmail.com>
2019-11-28 23:35:51 -0500

    Correct responsiveness and update portfolio with latest info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ProductsComponent } from './products/products.component';
 import { AboutComponent } from './about/about.component';
 import { ProductsService } from './products/products.service';
 import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { EducationComponent } from './education/education.component';
-import { AboutAppComponent } from './about-app/about-app.component';
+
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidenavService } from './sidenav/sidenav.service';
@@ -21,13 +20,11 @@ import { SidenavService } from './sidenav/sidenav.service';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: 'about', pathMatch: 'full' },
-  { path: 'products', component: ProductsComponent },
   { path: 'about', component: AboutComponent },
   { path: 'skills', component: SkillsComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'experience', component: ExperienceComponent },
-  { path: 'education', component: EducationComponent },
-  { path: 'aboutapp', component: AboutAppComponent }
+  { path: 'education', component: EducationComponent }
 ];
 
 
@@ -35,14 +32,12 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     HeaderComponent,
-    ProductsComponent,
     AboutComponent,
     SkillsComponent,
     ProjectsComponent,
     ExperienceComponent,
     EducationComponent,
     FooterComponent,
-    AboutAppComponent,
     SidenavComponent
   ],
   imports: [
